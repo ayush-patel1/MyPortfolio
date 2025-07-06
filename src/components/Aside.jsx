5099725992e32bafd9d49d68324255417d0a3eb0
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2025-07-06 23:51:32 +0530

    add my social handles

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,113 +3,122 @@ import { GiCalendar, GiClawSlashes, GiMailbox, GiMayanPyramid, GiPhone } from "r
 const Aside = () => {
   return (
     <aside className="sidebar">
-        <div className="sidebar-info">
-          <figure className="avatar-box">
-            <img
-              src={"/images/image.png"}
-              alt="Richard hanrick"
-              width="80"
-            />
-          </figure>
+      <div className="sidebar-info">
+        <figure className="avatar-box">
+          <img
+            src={"/images/image.png"}
+            alt="Ayush Patel"
+            width="80"
+          />
+        </figure>
 
-          <div className="info-content">
-            <h1 className="name" title="Richard hanrick">
-              Ayush Patel
-            </h1>
+        <div className="info-content">
+          <h1 className="name" title="Ayush Patel">
+            Ayush Patel
+          </h1>
 
-            <p className="title">Web developer</p>
-          </div>
-
-          <button className="info_more-btn" data-sidebar-btn>
-            <span>Show Contacts</span>
-
-            <GiClawSlashes />
-          </button>
+          <p className="title">Web Developer</p>
         </div>
 
-        <div className="sidebar-info_more">
-          <div className="separator"></div>
+        <button className="info_more-btn" data-sidebar-btn>
+          <span>Show Contacts</span>
+          <GiClawSlashes />
+        </button>
+      </div>
 
-          <ul className="contacts-list">
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiMailbox/>
-                
-              </div>
+      <div className="sidebar-info_more">
+        <div className="separator"></div>
 
-              <div className="contact-info">
-                <p className="contact-title">Email</p>
+        <ul className="contacts-list">
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiMailbox />
+            </div>
 
-                <a href="mailto: ayush005rkt@gmail.com" className="contact-link">
-                  ayush005rkt@gmail.com
-                </a>
-              </div>
-            </li>
-
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiPhone/>
-              </div>
-
-              <div className="contact-info">
-                <p className="contact-title">Phone</p>
-
-                <a href="tel:+919792050354" className="contact-link">
-                  +91-9792050354
-                </a>
-              </div>
-            </li>
-
-        <li className="contact-item">
-  <div className="icon-box">
-    <GiCalendar />
-  </div>
-
-  <div className="contact-info">
-    <p className="contact-title">Date of Birth</p>
-    <time dateTime="2005-10-26">October 26, 2005</time>
-  </div>
-</li>
-
-
-            <li className="contact-item">
-              <div className="icon-box">
-                <GiMayanPyramid/>
-                <ion-icon name="location-outline"></ion-icon>
-              </div>
-
-              <div className="contact-info">
-                <p className="contact-title">Location</p>
-
-                <address>Varanasi, India</address>
-              </div>
-            </li>
-          </ul>
-
-          <div className="separator"></div>
-
-          <ul className="social-list">
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-facebook"></ion-icon>
+            <div className="contact-info">
+              <p className="contact-title">Email</p>
+              <a href="mailto:ayush005rkt@gmail.com" className="contact-link">
+                ayush005rkt@gmail.com
               </a>
-            </li>
+            </div>
+          </li>
 
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-twitter"></ion-icon>
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiPhone />
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Phone</p>
+              <a href="tel:+919792050354" className="contact-link">
+                +91-9792050354
               </a>
-            </li>
+            </div>
+          </li>
 
-            <li className="social-item">
-              <a href="#" className="social-link">
-                <ion-icon name="logo-instagram"></ion-icon>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
-  )
-}
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiCalendar />
+            </div>
 
-export default Aside
+            <div className="contact-info">
+              <p className="contact-title">Date of Birth</p>
+              <time dateTime="2005-10-26">October 26, 2005</time>
+            </div>
+          </li>
+
+          <li className="contact-item">
+            <div className="icon-box">
+              <GiMayanPyramid />
+            </div>
+
+            <div className="contact-info">
+              <p className="contact-title">Location</p>
+              <address>Varanasi, India</address>
+            </div>
+          </li>
+        </ul>
+
+        <div className="separator"></div>
+
+        <ul className="social-list">
+          <li className="social-item">
+            <a
+              href="https://www.linkedin.com/in/ayush-patel05/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a
+              href="https://github.com/ayush-patel1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </li>
+
+          <li className="social-item">
+            <a
+              href="https://twitter.com/DevAyushX"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-link"
+            >
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
+
